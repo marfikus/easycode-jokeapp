8f5b81fc4e1407dcfa4fc20ace306e15a47585c1
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-28 15:19:28 +0500

    Rename Error to JokeFailure

--- app/src/main/java/com/github/marfikus/jokeapp/JokeFailure.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/JokeFailure.kt
@@ -1,16 +1,16 @@
 package com.github.marfikus.jokeapp
 
-interface Error {
+interface JokeFailure {
 
     fun getMessage(): String
 }
 
-class NoConnection(private val resourceManager: ResourceManager) : Error {
+class NoConnection(private val resourceManager: ResourceManager) : JokeFailure {
 
     override fun getMessage() = resourceManager.getString(R.string.no_connection)
 }
 
-class ServiceUnavailable(private val resourceManager: ResourceManager) : Error {
+class ServiceUnavailable(private val resourceManager: ResourceManager) : JokeFailure {
 
     override fun getMessage() = resourceManager.getString(R.string.service_unavailable)
 }
