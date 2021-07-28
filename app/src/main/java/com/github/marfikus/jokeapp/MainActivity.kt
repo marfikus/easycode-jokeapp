2ed704946fb01a85af4f9b430e4b6aa9edae494a
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-28 14:16:32 +0500

    Add MainActivity logic

--- app/src/main/java/com/github/marfikus/jokeapp/MainActivity.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/MainActivity.kt
@@ -2,10 +2,42 @@ package com.github.marfikus.jokeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = (application as JokeApp).viewModel
+        val button = findViewById<Button>(R.id.actionButton)
+        val textView = findViewById<TextView>(R.id.textView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        progressBar.visibility = View.INVISIBLE
+
+        button.setOnClickListener {
+            button.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+            viewModel.getJoke()
+        }
+
+        viewModel.init(object : TextCallback {
+            override fun provideText(text: String) {
+                button.isEnabled = true
+                progressBar.visibility = View.INVISIBLE
+                textView.text = text
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        viewModel.clear()
+        super.onDestroy()
     }
 }
