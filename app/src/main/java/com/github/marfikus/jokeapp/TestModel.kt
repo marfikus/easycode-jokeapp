dde89c77b14abb29226054c08cad41918c848237
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-28 15:42:02 +0500

    Update TestModel, Model, ViewModel, JokeApp

--- app/src/main/java/com/github/marfikus/jokeapp/TestModel.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/TestModel.kt
@@ -1,24 +1,26 @@
 package com.github.marfikus.jokeapp
 
-class TestModel : Model<Any, Any> {
+class TestModel(resourceManager: ResourceManager) : Model {
 
-    private var callback: ResultCallback<Any, Any>? = null
-
-    private var count = 1
+    private var callback: ResultCallback? = null
+    private var count = 0
+    private val noConnection = NoConnection(resourceManager)
+    private val serviceUnavailable = ServiceUnavailable(resourceManager)
 
     override fun getJoke() {
         Thread {
             Thread.sleep(1000)
-            if (count % 2 == 0) {
-                callback?.provideSuccess("success")
-            } else {
-                callback?.provideError("error")
+            when (count) {
+                0 -> callback?.provideSuccess(Joke("testText", "testPunchline"))
+                1 -> callback?.provideError(noConnection)
+                2 -> callback?.provideError(serviceUnavailable)
             }
             count++
+            if (count == 3) count = 0
         }.start()
     }
 
-    override fun init(callback: ResultCallback<Any, Any>) {
+    override fun init(callback: ResultCallback) {
         this.callback = callback
     }
 
