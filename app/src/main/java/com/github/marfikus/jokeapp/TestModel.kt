b402090a8127f8f62993daa1ac18dd170d54347b
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-28 14:44:44 +0500

    Add class TestModel

---
+++ app/src/main/java/com/github/marfikus/jokeapp/TestModel.kt
@@ -0,0 +1,26 @@
+package com.github.marfikus.jokeapp
+
+class TestModel : Model<Any, Any> {
+
+    private var callback: ResultCallback<Any, Any>? = null
+
+    private var count = 1
+
+    override fun getJoke() {
+        Thread.sleep(1000)
+        if (count % 2 == 0) {
+            callback?.provideSuccess("success")
+        } else {
+            callback?.provideError("error")
+        }
+        count++
+    }
+
+    override fun init(callback: ResultCallback<Any, Any>) {
+        this.callback = callback
+    }
+
+    override fun clear() {
+        callback = null
+    }
+}
