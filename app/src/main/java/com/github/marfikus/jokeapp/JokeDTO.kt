798a3925dd781dd3cd8523775d651d39819e165c
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-28 21:20:37 +0500

    Add Gson library, add class JokeDTO

---
+++ app/src/main/java/com/github/marfikus/jokeapp/JokeDTO.kt
@@ -0,0 +1,14 @@
+package com.github.marfikus.jokeapp
+
+import com.google.gson.annotations.SerializedName
+
+data class JokeDTO(
+    @SerializedName("id")
+    private val id: Int,
+    @SerializedName("type")
+    private val type: String,
+    @SerializedName("setup")
+    private val text: String,
+    @SerializedName("punchline")
+    private val punchline: String
+)
