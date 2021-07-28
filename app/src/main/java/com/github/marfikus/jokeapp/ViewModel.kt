ee6c048b53970c83595af9234083c11ac485fc7b
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-28 14:27:58 +0500

    Add ViewModel

---
+++ app/src/main/java/com/github/marfikus/jokeapp/ViewModel.kt
@@ -0,0 +1,33 @@
+package com.github.marfikus.jokeapp
+
+class ViewModel(private val model: Model<Any, Any>) {
+
+    private var callback: TextCallback? = null
+
+    fun init(callback: TextCallback) {
+        this.callback = callback
+        model.init(object : ResultCallback<Any, Any> {
+            override fun provideSuccess(data: Any) {
+                callback.provideText("success")
+            }
+
+            override fun provideError(error: Any) {
+                callback.provideText("error")
+            }
+        })
+    }
+
+    fun getJoke() {
+        model.getJoke()
+    }
+
+    fun clear() {
+        callback = null
+        model.clear()
+    }
+}
+
+interface TextCallback {
+
+    fun provideText(text: String)
+}
