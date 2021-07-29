60eea15ff949c35b285cb4b77d45a929b67de8e0
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-29 16:44:16 +0500

    Add class TestCacheDataSource

---
+++ app/src/main/java/com/github/marfikus/jokeapp/TestCacheDataSource.kt
@@ -0,0 +1,17 @@
+package com.github.marfikus.jokeapp
+
+class TestCacheDataSource : CacheDataSource {
+
+    private val map = HashMap<Int, JokeServerModel>()
+
+    override fun addOrRemove(id: Int, jokeServerModel: JokeServerModel): Joke {
+        return if (map.containsKey(id)) {
+            val joke = map[id]!!.toBaseJoke()
+            map.remove(id)
+            joke
+        } else {
+            map[id] = jokeServerModel
+            jokeServerModel.toFavoriteJoke()
+        }
+    }
+}
