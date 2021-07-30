abf0a8aee9682c21dd48f4d92f0154cf06d6fc1c
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-30 15:24:05 +0500

    Add class NoCachedJokes

--- app/src/main/java/com/github/marfikus/jokeapp/JokeFailure.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/JokeFailure.kt
@@ -14,3 +14,8 @@ class ServiceUnavailable(private val resourceManager: ResourceManager) : JokeFai
 
     override fun getMessage() = resourceManager.getString(R.string.service_unavailable)
 }
+
+class NoCachedJokes(private val resourceManager: ResourceManager) : JokeFailure {
+    override fun getMessage() = resourceManager.getString(R.string.no_cached_jokes)
+
+}
