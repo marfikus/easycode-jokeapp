c91d6b41dd9dd5a313d45adb2c379f1799112a1a
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-07-31 14:23:58 +0500

    Add interface ModelCallback, fix CacheDataSource

--- app/src/main/java/com/github/marfikus/jokeapp/CacheDataSource.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/CacheDataSource.kt
@@ -2,7 +2,9 @@ package com.github.marfikus.jokeapp
 
 interface CacheDataSource {
 
-    fun addOrRemove(id: Int, jokeServerModel: JokeServerModel): Joke
+    fun addOrRemove(id: Int,
+                    jokeServerModel: JokeServerModel,
+                    modelCallback: ModelCallback)
 
     fun getJoke(jokeCachedCallback: JokeCachedCallback)
 }
---
+++ app/src/main/java/com/github/marfikus/jokeapp/ModelCallback.kt
@@ -0,0 +1,6 @@
+package com.github.marfikus.jokeapp
+
+interface ModelCallback {
+
+    fun provide(joke: Joke)
+}
