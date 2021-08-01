a2bf96c4eab4de5fde2b8952ddcf441317e88dd8
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-01 15:09:08 +0500

    Fix getJoke() in ViewModel

--- app/src/main/java/com/github/marfikus/jokeapp/ViewModel.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/ViewModel.kt
@@ -2,6 +2,8 @@ package com.github.marfikus.jokeapp
 
 import androidx.annotation.DrawableRes
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class ViewModel(private val model: Model) : ViewModel() {
 
@@ -20,8 +22,11 @@ class ViewModel(private val model: Model) : ViewModel() {
         model.init(jokeCallback)
     }
 
-    fun getJoke() {
-        model.getJoke()
+    fun getJoke() = viewModelScope.launch {
+        val uiModel = model.getJoke()
+        dataCallback?.let {
+            uiModel.map(it)
+        }
     }
 
     fun changeJokeStatus() {
