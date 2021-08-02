ce79f473c29a42432fca66aa00befaac0e5d6e70
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-02 10:49:42 +0500

    Remake TestCloudDataSource

--- app/src/main/java/com/github/marfikus/jokeapp/TestCloudDataSource.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/TestCloudDataSource.kt
@@ -1,13 +1,15 @@
 package com.github.marfikus.jokeapp
 
-/*
 class TestCloudDataSource : CloudDataSource {
 
+    private var success = true
     private var count = 0
 
-    override fun getJoke(callback: JokeCloudCallback) {
-        val joke = JokeServerModel(count, "testType", "testText$count", "testPunchline$count")
-        callback.provide(joke.toJoke())
-        count++
+    override suspend fun getJoke(): Result<JokeServerModel, ErrorType> {
+        return if (success) {
+            Result.Success(JokeServerModel(count++, "testType", "testText$count", "testPunchline$count"))
+        } else {
+            Result.Error(ErrorType.NO_CONNECTION)
+        }
     }
-}*/
+}
