65b3b3d72578caab05abbbf6eeee55b0af52a4c1
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-02 23:23:58 +0500

    Refactor BaseModel: move BaseResultHandler, CacheResultHandler, CloudResultHandler to separate files

--- app/src/main/java/com/github/marfikus/jokeapp/BaseModel.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/BaseModel.kt
@@ -5,13 +5,10 @@ import kotlinx.coroutines.withContext
 
 class BaseModel(
     private val cacheDataSource: CacheDataSource,
-    cloudDataSource: CloudDataSource,
-    private val resourceManager: ResourceManager
+    private val cacheResultHandler: CacheResultHandler,
+    private val cloudResultHandler: CloudResultHandler,
+    private val cachedJoke: CachedJoke
 ) : Model {
-
-    private var cachedJoke: Joke? = null
-    private val cacheResultHandler by lazy { CacheResultHandler(cacheDataSource) }
-    private val cloudResultHandler = CloudResultHandler(cloudDataSource)
 
     private var currentResultHandler: BaseResultHandler<*, *> = cloudResultHandler
 
@@ -25,60 +22,6 @@ class BaseModel(
 
     override suspend fun changeJokeStatus(): JokeUiModel? =
         withContext(Dispatchers.IO) {
-            cachedJoke?.changeStatus(cacheDataSource)
+            cachedJoke.changeStatus(cacheDataSource)
         }
-
-
-    private abstract inner class BaseResultHandler<S, E>
-        (private val jokeDataFetcher: JokeDataFetcher<S, E>) {
-
-            suspend fun process(): JokeUiModel {
-                return handleResult(jokeDataFetcher.getJoke())
-            }
-
-            protected abstract fun handleResult(result: Result<S, E>): JokeUiModel
-        }
-
-    private inner class CloudResultHandler(jokeDataFetcher: JokeDataFetcher<JokeServerModel, ErrorType>) :
-            BaseResultHandler<JokeServerModel, ErrorType>(jokeDataFetcher) {
-
-        private val noConnection by lazy { NoConnection(resourceManager) }
-        private val serviceUnavailable by lazy { ServiceUnavailable(resourceManager) }
-
-        override fun handleResult(result: Result<JokeServerModel, ErrorType>) = when (result) {
-            is Result.Success<JokeServerModel> -> {
-                result.data.toJoke().let {
-                    cachedJoke = it
-                    it.toBaseJoke()
-                }
-            }
-            is Result.Error<ErrorType> -> {
-                cachedJoke = null
-                val failure = if (result.exception == ErrorType.NO_CONNECTION)
-                    noConnection
-                else
-                    serviceUnavailable
-                FailedJokeUiModel(failure.getMessage())
-            }
-        }
-
-    }
-
-    private inner class CacheResultHandler(jokeDataFetcher: JokeDataFetcher<Joke, Unit>) :
-            BaseResultHandler<Joke, Unit>(jokeDataFetcher) {
-
-        private val noCachedJokes by lazy { NoCachedJokes(resourceManager) }
-
-        override fun handleResult(result: Result<Joke, Unit>) = when (result) {
-            is Result.Success<Joke> -> result.data.let {
-                cachedJoke = it
-                it.toFavoriteJoke()
-            }
-            is Result.Error -> {
-                cachedJoke = null
-                FailedJokeUiModel(noCachedJokes.getMessage())
-            }
-        }
-
-    }
 }
---
+++ app/src/main/java/com/github/marfikus/jokeapp/CacheResultHandler.kt
@@ -0,0 +1,20 @@
+package com.github.marfikus.jokeapp
+
+class CacheResultHandler(
+    private val cachedJoke: CachedJoke,
+    jokeDataFetcher: JokeDataFetcher<Joke, Unit>,
+    private val noCachedJokes: JokeFailure
+) : BaseResultHandler<Joke, Unit>(jokeDataFetcher) {
+
+    override fun handleResult(result: Result<Joke, Unit>) = when (result) {
+        is Result.Success<Joke> -> result.data.let {
+            cachedJoke.saveJoke(it)
+            it.toFavoriteJoke()
+        }
+        is Result.Error -> {
+            cachedJoke.clear()
+            FailedJokeUiModel(noCachedJokes.getMessage())
+        }
+    }
+
+}
