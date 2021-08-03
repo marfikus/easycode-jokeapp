c165cd0552c048085f889e884f9615911eee01ce
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-03 15:55:43 +0500

    Add class BaseCommunication

---
+++ app/src/main/java/com/github/marfikus/jokeapp/BaseCommunication.kt
@@ -0,0 +1,18 @@
+package com.github.marfikus.jokeapp
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+
+class BaseCommunication : Communication {
+
+    private val liveData = MutableLiveData<Pair<String, Int>>()
+
+    override fun showData(data: Pair<String, Int>) {
+        liveData.value = data
+    }
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<Pair<String, Int>>) {
+        liveData.observe(owner, observer)
+    }
+}
