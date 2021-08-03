bab4f5ac3a3d141eb8112b401a440aa76591b64c
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-03 17:12:58 +0500

    Add dispatcher in BaseViewModel constructor

--- app/src/main/java/com/github/marfikus/jokeapp/BaseViewModel.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/BaseViewModel.kt
@@ -4,18 +4,21 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class BaseViewModel(
     private val model: Model,
-    private val communication: Communication
+    private val communication: Communication,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.Main
 ) : ViewModel() {
 
-    fun getJoke() = viewModelScope.launch {
+    fun getJoke() = viewModelScope.launch(dispatcher) {
         communication.showData(model.getJoke().getData())
     }
 
-    fun changeJokeStatus() = viewModelScope.launch {
+    fun changeJokeStatus() = viewModelScope.launch(dispatcher) {
         model.changeJokeStatus()?.let {
             communication.showData(it.getData())
         }
