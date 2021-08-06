90c3a2e47f1cecc731051a4e6257bb283ce2a4d4
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-06 14:22:00 +0500

    Add class JokeDataModel

---
+++ app/src/main/java/com/github/marfikus/jokeapp/data/JokeDataModel.kt
@@ -0,0 +1,9 @@
+package com.github.marfikus.jokeapp.data
+
+class JokeDataModel(
+        private val id: Int,
+        private val type: String,
+        private val text: String,
+        private val punchline: String,
+) {
+}
