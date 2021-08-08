05606fe3e648bada7695c2b75ed0b31905e736a0
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-08 15:56:04 +0500

    Update BaseCachedJoke

--- app/src/main/java/com/github/marfikus/jokeapp/BaseCachedJoke.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/BaseCachedJoke.kt
@@ -4,17 +4,17 @@ import com.github.marfikus.jokeapp.data.JokeDataModel
 
 class BaseCachedJoke : CachedJoke {
 
-    private var cached: JokeDataModel? = null
+    private var cached: ChangeJoke = ChangeJoke.Empty()
 
     override fun saveJoke(joke: JokeDataModel) {
         cached = joke
     }
 
     override fun clear() {
-        cached = null
+        cached = ChangeJoke.Empty()
     }
 
-    override suspend fun change(changeJokeStatus: ChangeJokeStatus): JokeDataModel? {
-        return cached?.change(changeJokeStatus)
+    override suspend fun change(changeJokeStatus: ChangeJokeStatus): JokeDataModel {
+        return cached.change(changeJokeStatus)
     }
 }
--- app/src/main/java/com/github/marfikus/jokeapp/ChangeJoke.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/ChangeJoke.kt
@@ -3,5 +3,12 @@ package com.github.marfikus.jokeapp
 import com.github.marfikus.jokeapp.data.JokeDataModel
 
 interface ChangeJoke {
-    suspend fun change(changeJokeStatus: ChangeJokeStatus): JokeDataModel?
+    suspend fun change(changeJokeStatus: ChangeJokeStatus): JokeDataModel
+
+    class Empty : ChangeJoke {
+        override suspend fun change(changeJokeStatus: ChangeJokeStatus): JokeDataModel {
+            return JokeDataModel(0, "", "")
+        }
+
+    }
 }
