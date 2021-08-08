0015f5a4c94969daec6a2504c9e35fe25001475f
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-08 15:42:36 +0500

    Update JokeDataModel

--- app/src/main/java/com/github/marfikus/jokeapp/data/JokeDataModel.kt
+++ app/src/main/java/com/github/marfikus/jokeapp/data/JokeDataModel.kt
@@ -1,9 +1,25 @@
 package com.github.marfikus.jokeapp.data
 
+import com.github.marfikus.jokeapp.ChangeJoke
+import com.github.marfikus.jokeapp.ChangeJokeStatus
+
 class JokeDataModel(
         private val id: Int,
-        private val type: String,
         val text: String,
         val punchline: String,
-) {
+        val cahched: Boolean = false
+) : ChangeJoke {
+
+    override suspend fun change(changeJokeStatus: ChangeJokeStatus) =
+            changeJokeStatus.addOrRemove(id, this)
+
+    fun toRealm() = JokeRealmModel().also { joke ->
+        joke.id = id
+        joke.text = text
+        joke.punchline = punchline
+    }
+
+    fun changeCached(cached: Boolean): JokeDataModel {
+        return JokeDataModel(id, text, punchline, cached)
+    }
 }
