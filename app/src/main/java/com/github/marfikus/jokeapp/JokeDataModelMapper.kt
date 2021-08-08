78268532eb3a238581315f841139bc765311de72
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-08-08 17:24:34 +0500

    Add JokeDataModelMapper

---
+++ app/src/main/java/com/github/marfikus/jokeapp/JokeDataModelMapper.kt
@@ -0,0 +1,23 @@
+package com.github.marfikus.jokeapp
+
+import com.github.marfikus.jokeapp.data.JokeRealmModel
+import com.github.marfikus.jokeapp.domain.Joke
+
+interface JokeDataModelMapper<T> {
+    fun map(id: Int, text: String, punchline: String, cached: Boolean): T
+}
+
+class JokeSuccessMapper : JokeDataModelMapper<Joke.Success> {
+    override fun map(id: Int, text: String, punchline: String, cached: Boolean) =
+        Joke.Success(text, punchline, cached)
+}
+
+class JokeRealmMapper : JokeDataModelMapper<JokeRealmModel> {
+    override fun map(id: Int, text: String, punchline: String, cached: Boolean): JokeRealmModel {
+        return JokeRealmModel().also { joke ->
+            joke.id = id
+            joke.text = text
+            joke.punchline = punchline
+        }
+    }
+}
